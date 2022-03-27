5d87c26c427728c4c103cada17657f97ef8644ea
Damjan Kalšan <damjankalsan@gmail.com>
2022-03-27 17:26:57 +0200

    Changed automation to use duration instead of wait time and removed start/stop command

--- helpers.js
+++ helpers.js
@@ -3,7 +3,6 @@ Adapted from https://github.com/JunShern/fish-stripes/
 */
 
 let autoInstructions = [];
-let validCommands = ['start', 'stop'];
 let instructionCounter = 1;
 let totalInstructions;
 
@@ -38,9 +37,8 @@ class Timer {
 
 
 class Instruction {
-    constructor(timer, command, numStripes, rotationSpeed, stripeColor1, stripeColor2) {
+    constructor(timer, numStripes, rotationSpeed, stripeColor1, stripeColor2) {
         this.timer = timer;
-        this.command = command;
         this.numStripes = numStripes;
         this.rotationSpeed = rotationSpeed;
         this.stripeColor1 = stripeColor1;
@@ -64,28 +62,24 @@ function startAutomation() {
             // Timer
             alert(`Error on line ${i + 1} "${commandString}":\n${command[0]} is not a valid number`);
         }
-        if (!validCommands.includes(command[1])) {
-            // Command
-            alert(`Error on line ${i + 1} "${commandString}":\n${command[1]} is not a valid command. Should be one of: ${validCommands}`);
+        if (isNaN(Number(command[1]))) {
+            // Number of stripes
+            alert(`Error on line ${i + 1} "${commandString}":\n${command[1]} is not a valid number`);
         }
         if (isNaN(Number(command[2]))) {
-            // Number of stripes
+            // Rotation speed
             alert(`Error on line ${i + 1} "${commandString}":\n${command[2]} is not a valid number`);
         }
-        if (isNaN(Number(command[3]))) {
-            // Rotation speed
-            alert(`Error on line ${i + 1} "${commandString}":\n${command[3]} is not a valid number`);
+        if (!(/^#?([a-f\d]{3,4}|[a-f\d]{6}|[a-f\d]{8})$/i.test(command[3]))) {
+            // Stripe color 1
+            alert(`Error on line ${i + 1} "${commandString}":\n${command[3]} is not a valid HEX string`);
         }
         if (!(/^#?([a-f\d]{3,4}|[a-f\d]{6}|[a-f\d]{8})$/i.test(command[4]))) {
-            // Stripe color 1
+            // Stripe color 2
             alert(`Error on line ${i + 1} "${commandString}":\n${command[4]} is not a valid HEX string`);
         }
-        if (!(/^#?([a-f\d]{3,4}|[a-f\d]{6}|[a-f\d]{8})$/i.test(command[5]))) {
-            // Stripe color 2
-            alert(`Error on line ${i + 1} "${commandString}":\n${command[5]} is not a valid HEX string`);
-        }
 
-        instruction = new Instruction(Number(command[0]), command[1], Number(command[2]), Number(command[3]), command[4], command[5]);
+        instruction = new Instruction(Number(command[0]), Number(command[1]), Number(command[2]), command[3], command[4]);
         autoInstructions.push(instruction);
     }
     instructionCounter = 1;
@@ -101,46 +95,36 @@ function executeNextInstruction() {
         return;
     }
 
-    nextInstruction = autoInstructions.shift();
-    autoStatus.html(`(${instructionCounter} / ${totalInstructions})<br>${nextInstruction.timer}s before executing "${nextInstruction.command}" with<br>
-    numStripes=${nextInstruction.numStripes},<br>rotationSpeed=${nextInstruction.rotationSpeed},<br>
-    stripeColor1=${nextInstruction.stripeColor1},<br>stripeColor2=${nextInstruction.stripeColor2}.`);
+    instruction = autoInstructions.shift();
+    autoStatus.html(`(${instructionCounter} / ${totalInstructions})<br>${instruction.timer}s remaining with configuration:<br>
+    numStripes=${instruction.numStripes},<br>rotationSpeed=${instruction.rotationSpeed},<br>
+    stripeColor1=${instruction.stripeColor1},<br>stripeColor2=${instruction.stripeColor2}.`);
 
-    console.log("Executing", nextInstruction)
+    console.log("Executing", instruction)
+
+    numStripes = instruction.numStripes;
+    numStripesSlider.value(numStripes);
+    numStripesInput.value(numStripes);
+
+    rotationSpeed = instruction.rotationSpeed;
+    rotationSpeedSlider.value(rotationSpeed);
+    rotationSpeedInput.value(rotationSpeed);
+
+    stripeColor1 = instruction.stripeColor1;
+    stripeColorInput1.html(stripeColor1);
+
+    stripeColor2 = instruction.stripeColor2;
+    stripeColorInput1.html(stripeColor2);
 
     let timer = new Timer();
     timer.start(
-        seconds = nextInstruction.timer,
+        seconds = instruction.timer,
         updateCallback = (count) => {
-            autoStatus.html(`(${instructionCounter} / ${totalInstructions})<br>${count}s before executing "${nextInstruction.command}" with<br>
-                numStripes=${nextInstruction.numStripes},<br>rotationSpeed=${nextInstruction.rotationSpeed},<br>
-                stripeColor1=${nextInstruction.stripeColor1},<br>stripeColor2=${nextInstruction.stripeColor2}.`);
+            autoStatus.html(`(${instructionCounter} / ${totalInstructions})<br>${count}s remaining with configuration:<br>
+                numStripes=${instruction.numStripes},<br>rotationSpeed=${instruction.rotationSpeed},<br>
+                stripeColor1=${instruction.stripeColor1},<br>stripeColor2=${instruction.stripeColor2}.`);
         },
         endCallback = () => {
-            numStripes = nextInstruction.numStripes;
-            numStripesSlider.value(numStripes);
-            numStripesInput.value(numStripes);
-
-            rotationSpeed = nextInstruction.rotationSpeed;
-            rotationSpeedSlider.value(rotationSpeed);
-            rotationSpeedInput.value(rotationSpeed);
-
-            stripeColor1 = nextInstruction.stripeColor1;
-            stripeColorInput1.html(stripeColor1);
-
-            stripeColor2 = nextInstruction.stripeColor2;
-            stripeColorInput1.html(stripeColor2);
-
-            switch (nextInstruction.command) {
-                case 'start':
-                    playStopButton.html("Pause");
-                    play = true;
-                    break;
-                case 'stop':
-                    playStopButton.html("Resume");
-                    play = false;
-                    break;
-            }
             instructionCounter++;
             executeNextInstruction();
         }
